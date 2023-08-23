e22676982a9a68123952ff862da074a3f2fcd16c
leoojg <leoojg@gmail.com>
2023-08-23 13:14:32 -0300

    feat: add tracking to project

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,22 @@
+import {Button, ButtonProps} from '@design-system/react'
+import type { StoryObj, Meta} from '@storybook/react'
+
+
+export default {
+  title: 'Button',
+  component: Button,
+  args: {
+    children: 'Enviar'
+  }
+} as Meta<ButtonProps>;
+
+export const Primary: StoryObj<ButtonProps> = {
+
+}
+
+
+export const Big: StoryObj<ButtonProps> = {
+  args: {
+    size: 'big'
+  }
+}
